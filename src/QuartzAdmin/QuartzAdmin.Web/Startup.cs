374fbe6dcc0c8268617cd08f3e7760088a283f9e
Trinath Mallavarapu <trinath@keka.com>
2017-09-28 14:13:18 +0530

    added mvc web project for scheduler admin portal

---
+++ src/QuartzAdmin/QuartzAdmin.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuartzAdmin.Web.Startup))]
+namespace QuartzAdmin.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
